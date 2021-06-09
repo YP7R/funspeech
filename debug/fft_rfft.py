25266b110192c1439e64561c2ad29527a2ee773a
YP7R <>
2021-06-09 15:39:32 +0200

    Energies features done

--- debug/fft_rfft.py
+++ debug/fft_rfft.py
@@ -1,38 +1,41 @@
 from pydub import AudioSegment
 import librosa
-import features.feature_band_power as bp
+import features.energy_features as bp
 import numpy as np
 
 filepath = "./../dataset/processed/funspeech/muser1-093623a.wav"
 
 signal, sr = librosa.load(filepath, sr=16000)
-signal = bp.pad_signal(signal)
-signal = bp.normalize_volume(signal)
-bp.compute_fft(signal)
-bp.compute_rfft(signal)
 
 print("*" * 100)
-sig = np.array([0, 1, 2, 0, 1, 1, 1, 1])
+sig = np.array([0, 1, 2, 0, 1, 1])
 
-sig =signal
+sig  # = signal
 
-'''
+# If signal is loaded from librosa, compute energies
+if sig.size > 10:
+    print(bp.energies_algorithm(sig, 16000))
+    exit(0)
+
+#######################################################################
+# FFT VS RFFT
 f = np.fft.fft(sig)
 r = np.fft.rfft(sig)
-#######################################################################
 print("FFT")
 # print(' | '.join(map(str, f))) # Must be commented, complete fft
 print(' | '.join(map(str, f[0:len(sig) // 2 + 1])))
-print(' | '.join(map(str, bp.compute_fft(sig))))
-print(bp.compute_fft(sig).size, f[0:len(sig)//2+1].size)
+print(' | '.join(map(str, bp._compute_fft(sig))))
+print(bp._compute_fft(sig).size, f[0:len(sig)//2+1].size)
 
 print("RFFT")
 print(' | '.join(map(str, r)))
-print(' | '.join(map(str, bp.compute_rfft(sig))))
-print(bp.compute_rfft(sig).size, r.size)
+print(' | '.join(map(str, bp._compute_rfft(sig))))
+print(bp._compute_rfft(sig).size, r.size)
 
 #######################################################################
+# FFT FREQ vs RFFT FREQ
 print("*" * 100)
+
 
 ff = np.fft.fftfreq(len(sig), 1 / 16000)
 rf = np.fft.rfftfreq(len(sig), 1 / 16000)
@@ -40,11 +43,20 @@ rf = np.fft.rfftfreq(len(sig), 1 / 16000)
 print("FFT FREQ")
 print(ff[0], ff[len(sig) // 2], f"Size: {ff.size}")
 print(' | '.join(map(str, np.abs(ff[0:len(sig) // 2 + 1]))))
-print(' | '.join(map(str, bp.compute_fftfreq(sig, 16000))))
+print(' | '.join(map(str, bp._compute_fftfreq(sig, 16000))))
 
 print("RFFT FREQ")
 print(rf[0], rf[-1], f"Size: {rf.size}")
 print(' | '.join(map(str, rf)))
-print(' | '.join(map(str, bp.compute_rfftfreq(sig, 16000))))
-'''
-bp.algorithm(sig, 16000)
+print(' | '.join(map(str, bp._compute_rfftfreq(sig, 16000))))
+
+#######################################################################
+# Différence entre un signal paddé et non paddé
+print("*" * 100)
+
+
+padded_sig = bp._pad_signal(sig)
+fft_values_pad = np.fft.rfft(padded_sig)
+fft_values = np.fft.rfft(sig)
+print(' | '.join(map(str, fft_values)))
+print(' | '.join(map(str, fft_values_pad)))
--- features/energy_features.py
+++ features/energy_features.py
@@ -10,55 +10,63 @@ import matplotlib.pyplot as plt
 # - Energy per bands
 '''
 
-
-def normalize_volume(signal):
+# Normalise le volume
+def _normalize_volume(signal):
     return np.sqrt(np.sum(np.square(signal)) / len(signal)) * signal
 
 
-def pad_signal(signal):
+# Pad un signal, tel qu'il soit puissance de deux
+def _pad_signal(signal):
     nb_zeros = int(np.power(2, np.ceil(np.log2(signal.size))) - signal.size)
     signal_padded = np.pad(signal, pad_width=(0, nb_zeros), mode='constant')
     return signal_padded
 
 
-def compute_fft(signal):
+# Calcule la (r)fft
+def _compute_fft(signal):
     fft_values = np.fft.fft(signal)
     # print(f"FFT size: {fft_values.size}, size if trunc {fft_values[0:len(signal)//2+1].size}")
     values = fft_values[0:len(signal) // 2 + 1]
     return values
 
 
-def compute_rfft(signal):
+# Calcule la fft
+def _compute_rfft(signal):
     fft_values = np.fft.rfft(signal)
     # print(f"RFFT size: {fft_values.size}")
     return fft_values
 
 
-def compute_fftfreq(signal, sr):
+# Calcule les (r)fréquences
+def _compute_fftfreq(signal, sr):
     fft_freq = np.fft.fftfreq(len(signal), 1 / sr)
     freq = np.abs(fft_freq[0:len(signal) // 2 + 1])
     return freq
 
 
-def compute_rfftfreq(signal, sr):
+# Calcule les (r)fréquences
+def _compute_rfftfreq(signal, sr):
     freq = np.fft.rfftfreq(len(signal), 1 / sr)
     return freq
 
 
-def compute_fft_log(fft_values):
+# Applique le logaritme a une fft
+def _compute_fft_log(fft_values):
     fft_log_values = np.where(fft_values == 0, np.finfo(float).eps, fft_values)
     fft_log_values = np.log(fft_log_values)
     return fft_log_values
 
 
-def hz2bin(freq, bands):
+# Convertit des hertz en indices
+def _hz2bin(freq, bands):
     delta = (freq[1] - freq[0])
     indices = np.array(np.floor(bands / delta), dtype=np.int)
     return indices
 
 
-def compute_power_per_bands(freq, fft_values, bands):
-    bands_indices = hz2bin(freq, bands)
+# Calcule l'energie par bandes
+def _compute_energy_per_bands(freq, fft_values, bands):
+    bands_indices = _hz2bin(freq, bands)
     features = []
     for index, bi in enumerate(bands[0:-1]):
         start, stop = bands_indices[index], bands_indices[index + 1]
@@ -67,38 +75,36 @@ def compute_power_per_bands(freq, fft_values, bands):
         energy = np.sum(np.power(values, 2))
         features.append(energy)
 
-    features = np.array(features) / len(features)
-    print(features)
+    features = np.array(features) / np.sum(features)
     return features
 
 
-# ...
-
-
-def algorithm(signal, sample_rate, bands=np.array([150, 551, 1051, 1901, 3501, 6501])):
+def energies_algorithm(signal, sample_rate, bands=np.array([150, 551, 1051, 1901, 3501, 6501])):
     # Normalise le volume
-    signal_normalized = normalize_volume(signal)
+    signal_normalized = _normalize_volume(signal)
 
     # Pad le signal
-    signal_padded = pad_signal(signal_normalized)
+    signal_padded = _pad_signal(signal_normalized)
 
     # Compute la fft
-    fft_values = compute_fft(signal_padded)
-    # rfft_values = compute_rfft(signal)
-    freq = compute_fftfreq(signal_padded, sample_rate)
-    # rfreq = compute_rfftfreq(signal, sample_rate)
+    fft_values = _compute_fft(signal_padded)
+    ## rfft_values = _compute_rfft(signal) # Autre façon
+    freq = _compute_fftfreq(signal_padded, sample_rate)
+    ## rfreq = _compute_rfftfreq(signal, sample_rate) # Autre façon
+    # print(' | '.join(map(str, fft_values)))
+    # print(' | '.join(map(str, freq)))
 
     # Compute la norme / magnitude
     fft_values = np.absolute(fft_values)
 
     # Applique le logarithme
-    fft_log_values = compute_fft_log(fft_values)
+    fft_log_values = _compute_fft_log(fft_values)
 
     # Normalisation
     fft_normalized_values = fft_log_values - np.min(fft_log_values)
 
     # Compute features ...
-    powers = compute_power_per_bands(freq, fft_normalized_values, bands)
-    # print(' | '.join(map(str, fft_values)))
-    # print(' | '.join(map(str, freq)))
-    # print(hz2bin(freq, bands))
+    energies = _compute_energy_per_bands(freq, fft_normalized_values, bands)
+    return energies
+
+
