b01eab0579801227fdf5375ab9896a515d363e2a
YP7R <>
2021-06-10 11:53:54 +0200

    main template

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import sys
+import os
+import glob
+'''
+# template
+# for csv in csvs:
+
+    # for clf in clfs:
+    
+        # for npy in npy:
+        
+            # learning curve
+            # save result +/ model
+            # ...
+'''
 print(sys.version)
